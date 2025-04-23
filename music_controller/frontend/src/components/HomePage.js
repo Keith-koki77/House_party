002530a606355e15f40cc992a29ced693436c077
Keit Koki <kokikeith@gmail.com>
2025-04-23 13:23:37 +0300

    Updated the serailizers.py

--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -1,11 +1,28 @@
 import React, { Component } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Navigate
+} from "react-router-dom";
+import RoomJoinPage    from "./RoomJoinPage";
+import CreateRoomPage  from "./CreateRoomPage";
 
 export default class HomePage extends Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return <p>This is the home page.</p>
-    }
+  render() {
+    return (
+      <Router>
+        <nav>
+          <Link to="/">Home</Link> | <Link to="/join">Join</Link> | <Link to="/create">Create</Link>
+        </nav>
+        <Routes>
+          <Route path="/"       element={<p>This is the home page</p>} />
+          <Route path="/join"   element={<RoomJoinPage />} />
+          <Route path="/create" element={<CreateRoomPage />} />
+          <Route path="*"       element={<Navigate to="/" replace />} />
+        </Routes>
+      </Router>
+    );
+  }
 }

4a07361043ca67b71e9ad07631e52c01091f25fa
Keit Koki <kokikeith@gmail.com>
2025-04-23 14:43:25 +0300

    fixed views.py

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -14,7 +14,7 @@ class CreateRoomView(APIView):
     serializer_class = CreateRoomSerializer
     
     def post(self, request, format=None):
-        if not self.request.session.exists(self.request.session.session_key_key):
+        if not self.request.session.exists(self.request.session.session_key):
             self.request.session.create()
             
         serializer = self.serializer_class(data=request.data)
